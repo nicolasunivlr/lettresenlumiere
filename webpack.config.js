864167c16002862697cd43909835b82b9791a5d5
jbossisg <jbossisg@DEV1.ctia.local>
2026-03-11 12:09:06 +0100

    fix route build mais ça ne devrait pas être a fix

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 if (!Encore.isProduction()) {
     // public path used by the web server to access the output path
     Encore.setOutputPath('public/build/')
-        .setPublicPath('/build')
+        .setPublicPath('/lettresenlumiere/build')
 } else {
     Encore.setOutputPath('public/build/')
         .setPublicPath('/lettresenlumiere/build')
